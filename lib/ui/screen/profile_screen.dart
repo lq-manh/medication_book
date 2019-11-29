a6729ab1dd9edb07a4ee9daf16a1a53f7fee1ec3
Luong Quang Manh <kysumot@teneocto.io>
2019-11-29 15:50:06 +0700

    Complete profile screen

--- lib/ui/screen/profile_screen.dart
+++ lib/ui/screen/profile_screen.dart
@@ -27,16 +27,72 @@ class ProfileScreen extends StatelessWidget {
       main: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-          // child: RoundedCard(child: _Profile()),
+          child: _ProfileCard(),
         ),
       ),
     );
   }
 }
 
-class _Profile extends StatelessWidget {
+class _ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(height: 500);
+    return RoundedCard(
+      hasBorder: true,
+      hasShadow: false,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          children: <Widget>[
+            _InfoRow(fieldName: 'Name', value: 'Luong Quang Manh'),
+            _InfoRow(fieldName: 'Age', value: '21'),
+            _InfoRow(fieldName: 'Gender', value: 'Male'),
+            _InfoRow(fieldName: 'Height', value: '168', unit: 'cm'),
+            _InfoRow(fieldName: 'Weight', value: '48', unit: 'kg'),
+            _InfoRow(fieldName: 'Blood Type', value: 'B'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _InfoRow extends StatelessWidget {
+  final String fieldName;
+  final String value;
+  final String unit;
+
+  _InfoRow({@required this.fieldName, @required this.value, this.unit = ''});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 100,
+            child: Text(
+              this.fieldName,
+              style: TextStyle(color: ColorPalette.blue),
+            ),
+          ),
+          Text(
+            this.value,
+            style: TextStyle(
+              color: ColorPalette.textBody,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            this.unit,
+            style: TextStyle(
+              color: ColorPalette.textBody,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
